1183e95f21c9291886cbc809a4cda0452ec7a7f3
Kamer Berat Topal <kamertopalxp@gmail.com>
2021-05-05 19:35:50 +0300

    Some bug fixes and updates

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -21,7 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = ***
+SECRET_KEY = 'fjkdsdfhshgdjshgdfshkfsahfghsdhjg'
+
+def get_random_secret_key():
+    """
+    Return a 50 character random string usable as a SECRET_KEY setting value.
+    """
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    return get_random_string(50, chars)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
